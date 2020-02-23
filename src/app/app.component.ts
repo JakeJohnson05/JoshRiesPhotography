7fcf84d70c9dfa2a89aec034de3909c81582250b
Jake Johnson <jakejohnson801@outlook.com>
2020-02-23 16:07:10 -0700

    Updating angular/cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 import { slideInAnimation } from './animations';
 
+// import { ContactService } from './contact/contact.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +12,12 @@ import { slideInAnimation } from './animations';
   animations: [slideInAnimation]
 })
 export class AppComponent {
+  constructor(
+    // private contactService: ContactService
+  ) {
+    // this.contactService.recentEmailStatus;
+  }
+  
   /** Prepares and sends route data for animations */
   prepareRoute = (outlet: RouterOutlet) => outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
 }
